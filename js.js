0c08b4c8cad96b8e778338cc6c48c2289c95737d
rashidiqbalnit <125125257+rashidiqbalnit@users.noreply.github.com>
2023-02-27 11:59:11 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+//console.log("Hi Rashid");
+//var myName="Rashid";
+/*let myName="Rashid",yourName="Development";
+console.log("muName is "+myName+ " , welcome to my course "+yourName+".");*/
+/*let a=5,b=7;
+console.log("sum of "+a+ " , and "+b+" is = "+(a+b)+"");*/
+
+// var is old variable decleration type while let is new variable decleration.
+let a;
+a=1;
+a=2;
+a=3;
+alert("your level "+a);
---
+++ st.jsx
@@ -0,0 +1,14 @@
+/*alert("hy");*/
+function cal(n1,n2)
+{
+    return n1*n2;
+}
+cal(5,8);
+/*let numberOfDrumButtons=document.querySelectorAll(".drum").length;
+
+for(let i=0;i<numberOfDrumButtons;i++)
+{
+document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+alert("I got clicked!");
+});
+}
